b784ac6f62fb1f72517a392518da9fd419af8877
swaytuned <swaytuned805@gmail.com>
2022-07-01 01:19:41 -0700

    Corrections to naming of class and id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let passwordOne = document.getElementById("pwOne")
-let passwordTwo = document.getElementById("pwTwo")
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
 let generated = ""
 
 
@@ -21,9 +21,9 @@ function randomizeCharacters(){
 }
 
 //function to generate passwords
-//function pwbtn(){
+//function generatePassword(){
 
- }
+ //}
 //console.log(randomizeCharacters())
 
 //for (let i = 0; i < characters.length; i++)
