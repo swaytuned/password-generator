6ff071520257737b6777210aee8dfb4d6013bafd
swaytuned <swaytuned805@gmail.com>
2022-07-01 18:28:39 -0700

    Relocated math.random

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let passwordOne = document.getElementById("password-one")
-let passwordTwo = document.getElementById("password-two")
-let passwordButton = document.getElementById("password-btn")
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+let passwordButton = document.getElementById("password-btn");
+let empty ="";
 
 
-
-//function to call array
+//Onclick to generate random characters from array
 function generatePassword(){
     // math to pull from array, need two
-    let randomCharacter = Math.floor( Math.random() * characters.length ) 
-  
-    //pull random character for each password box
     
-    //passwordTwo.textContent = characters[randomCharacter]
+    //let randomCharacterTwo = Math.floor( Math.random() * characters.length ) 
+    //passwordTwo.textContent = characters[randomCharacterTwo]
 
-    //for loop
+    //adds in fifteen chracters
     for (let i = 0; i < 15; i++){
-        passwordOne.textContent += characters[randomCharacter]
+        let randomCharacterOne = Math.floor( Math.random() * characters.length ) 
+        passwordOne.textContent += characters[randomCharacterOne]  
     }
     
 }
@@ -28,6 +27,4 @@ function generatePassword(){
  
 
 
-//for (let i = 0; i < characters.length; i++)
-//console.log(characters[i])
 
