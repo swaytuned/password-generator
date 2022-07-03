1f35d5578b6f2d5bf37e3c42bce89779143332b3
swaytuned <swaytuned805@gmail.com>
2022-07-02 23:24:47 -0700

    Refactored code

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 'use strict';
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let passwordOne = document.getElementById("password-one");
-let passwordTwo = document.getElementById("password-two");
-let passwordButton = document.getElementById("password-btn");
-let newPassword = "";
+const passwordOne = document.getElementById("password-one");
+const passwordTwo = document.getElementById("password-two");
+const passwordButton = document.getElementById("password-btn");
+
 
 
 //Onclick to generate random characters from array
 function generatePassword(){   
-        //Start with empty text field
-        passwordOne.textContent = newPassword
-        passwordTwo.textContent = newPassword
-    //adds in fifteen chracters
+    //Start with empty text field step 1
+    let newPasswordOne = "";  
+    let newPasswordTwo = "";     
+    //adds in fifteen chracters step 2
     for (let i = 0; i < 15; i++){
         //randomizes the array to output passwords
         let randomCharacterOne = Math.floor( Math.random() * characters.length ) 
         let randomCharacterTwo = Math.floor( Math.random() * characters.length ) 
         //Randomized chracters and outputs to  text field
-        passwordOne.textContent += characters[randomCharacterOne] 
-        passwordTwo.textContent += characters[randomCharacterTwo]
+        newPasswordOne += characters[randomCharacterOne]
+        newPasswordTwo += characters[randomCharacterTwo]
+        //step 3
+        passwordOne.textContent = newPasswordOne;
+        passwordTwo.textContent = newPasswordTwo;
     }       
 
 }
+
